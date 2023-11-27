d9bc1b1e472bdfa840cd69c9d2051b44ce1334df
Ethan Hu <ethanhu8351@gmail.com>
2023-11-26 23:39:04 -0800

    added comment to wavelengthToRGB.m to indicate colors

--- software/wavelengthToRGB.m
+++ software/wavelengthToRGB.m
@@ -6,31 +6,30 @@
 % output: RGB - color in RGB format
 %
 function RGB = wavelengthToRGB(wavelength)
-        gamma = 0.80;
-
-    if wavelength >= 380 && wavelength < 440
+    gamma = 0.80;
+    if wavelength >= 380 && wavelength < 440 % violet
         attenuation = 0.3 + 0.7 * (wavelength - 380) / (440 - 380);
-        r = ((-(wavelength - 440) / (440 - 380)) * attenuation) ^ gamma;
+        r = ((-(wavelength - 440) / (440 - 380)) * attenuation) ^ gamma; % fade out when close to 380
         g = 0.0;
         b = (1.0 * attenuation) ^ gamma;
-    elseif wavelength >= 440 && wavelength < 490
+    elseif wavelength >= 440 && wavelength < 490 % blue
         r = 0.0;
         g = ((wavelength - 440) / (490 - 440)) ^ gamma;
         b = 1.0;
-    elseif wavelength >= 490 && wavelength < 510
+    elseif wavelength >= 490 && wavelength < 510 % green
         r = 0.0;
         g = 1.0;
         b = (-(wavelength - 510) / (510 - 490)) ^ gamma;
-    elseif wavelength >= 510 && wavelength < 580
+    elseif wavelength >= 510 && wavelength < 580 % yellow
         r = ((wavelength - 510) / (580 - 510)) ^ gamma;
         g = 1.0;
         b = 0.0;
-    elseif wavelength >= 580 && wavelength < 645
+    elseif wavelength >= 580 && wavelength < 645 % red
         r = 1.0;
         g = (-(wavelength - 645) / (645 - 580)) ^ gamma;
         b = 0.0;
-    elseif wavelength >= 645 && wavelength < 781
-        attenuation = 0.3 + 0.7 * (780 - wavelength) / (780 - 645);
+    elseif wavelength >= 645 && wavelength < 781 % infrared
+        attenuation = 0.3 + 0.7 * (780 - wavelength) / (780 - 645); % fade out when close to 780
         r = (1.0 * attenuation) ^ gamma;
         g = 0.0;
         b = 0.0;
